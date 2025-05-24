c080ff6811a302413131e34cbd1b1db317a81113
AMMAAR-IC <the.ammaar.ic@gmail.com>
2025-05-24 14:15:21 -0400

    Create SimpleCal.py

---
+++ SimpleCal.py
@@ -0,0 +1,32 @@
+print("Please write in this format: '{number}' '{operator}' '{number}'")
+
+while True:
+    try:
+        a = input("➤ ")
+        one, two, three = a.split()
+
+        n1 = int(one)
+        op = two
+        n2 = int(three)
+
+        if op == "+":
+            res = n1 + n2
+        elif op == "-":
+            res = n1 - n2
+        elif op == "*":
+            res = n1 * n2
+        elif op == "/":
+            res = n1 / n2
+        elif op == "%":
+            res = n1 % n2
+        else:
+            print("❌ Invalid operator! Use +, -, *, /, or %.")
+            continue
+
+        print("✅ Result:", res)
+        break
+
+    except ValueError:
+        print("❌ Please enter valid numbers!")
+    except ZeroDivisionError:
+        print("❌ Cannot divide by zero!")
