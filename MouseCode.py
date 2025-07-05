939402fbfde60ccc9477b8e06a3c4c7a38bf8773
AMMAAR BAKSHI <the.ammaar.bakshi@gmail.com>
2025-07-05 23:10:14 +0530

    Create MouseCode.py

---
+++ MouseCode.py
@@ -0,0 +1,13 @@
+def text_to_morse(text):
+    morse_dict = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+        'Z': '--..', ' ': '/'
+    }
+    return ' '.join(morse_dict.get(c.upper(), '') for c in text)
+
+msg = input("Enter a message: ")
+print("Morse Code:", text_to_morse(msg))
