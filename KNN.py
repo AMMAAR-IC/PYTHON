900c5b0b26ddd7a5fcd140641f74915e77a865d5
AMMAAR BAKSHI <the.ammaar.ic@gmail.com>
2025-07-14 20:49:08 +0530

    Create KNN.py

---
+++ KNN.py
@@ -0,0 +1,19 @@
+import numpy as np
+from collections import Counter
+
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        return [self._predict_point(x) for x in X]
+
+    def _predict_point(self, x):
+        distances = [np.linalg.norm(x - x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_labels = [self.y_train[i] for i in k_indices]
+        return Counter(k_labels).most_common(1)[0][0]
